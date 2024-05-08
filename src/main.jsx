bc3f3782ae01401ec4e75cbd9427586d1bec53a8
JoaoVictorFerrer <137827716+JoaoVictorFerrer@users.noreply.github.com>
2024-05-08 19:50:11 +0200

    actualizando la rama main

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App.jsx";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "./index.css";
 import { main_theme } from "../palette-theme-colors.js";
-import ReactDOM from "react-dom/client";
-import { CssBaseline } from "@mui/material";
-import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
