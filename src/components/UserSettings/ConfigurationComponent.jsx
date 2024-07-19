dc2813595c9b75b5f3aae58de03dedc29c98d77f
JoaoVictorFerrer <137827716+JoaoVictorFerrer@users.noreply.github.com>
2024-07-19 13:43:03 +0200

    gestion de alertas para eliminar cuenta

--- src/components/UserSettings/ConfigurationComponent.jsx
+++ src/components/UserSettings/ConfigurationComponent.jsx
@@ -20,6 +20,7 @@ import { UserContext } from '../../context/userContext';
 import { deleteUser } from '../../apiServices/usersApi';
 import { useNavigate } from 'react-router-dom';
 import { LoginContextP } from '../../context/loginContextPrueba';
+import AlertDelete from './AlertDelete';
 
 export default function ConfigurationComponent({
    values,
@@ -36,6 +37,17 @@ export default function ConfigurationComponent({
    const [showPassword, setShowPassword] = useState(false);
    const [showPasswordC, setShowPasswordC] = useState(false);
    const [showPassworNew, setShowPassworNew] = useState(false);
+   const [openAlertDelete, setOpenAlertDelete] = useState(false);
+   const [loadingDelete, setLoadingDelete] = useState(false);
+   const [answerDelete, setAnswerDelete] = useState(null);
+
+   const handleOpenAlertDelete = () => {
+      setOpenAlertDelete(true);
+   };
+
+   const handleCloseAlertDelete = () => {
+      setOpenAlertDelete(false);
+   };
    const handleClickShowPassword = () => setShowPassword((show) => !show);
    const handleClickShowPasswordC = () => setShowPasswordC((show) => !show);
    const handleClickShowPasswordNew = () => setShowPassworNew((show) => !show);
@@ -58,10 +70,15 @@ export default function ConfigurationComponent({
    }
 
    const deleteMyAccount = async () => {
+      setLoadingDelete(true)
       const response = await deleteUser(profileDetails._id)
       if (response.data) {
-         console.log('esto es response data',response.data)
-         navigate('/')
+         setLoadingDelete(false)
+         setAnswerDelete(true)
+         localStorage.removeItem('access_token')
+      } else {
+         setLoadingDelete(false)
+         setAnswerDelete(false)
       }
    }
 
@@ -73,6 +90,7 @@ export default function ConfigurationComponent({
             
          }}
       >
+         <AlertDelete deleteMyAccount={deleteMyAccount} handleCloseAlertDelete={handleCloseAlertDelete} openAlertDelete={openAlertDelete} loadingDelete={loadingDelete} answerDelete={answerDelete} />
          <Grid  minWidth='210%'sx={{display:'flex',flexDirection:'column',mt:'2rem'}} ml={'2.7rem'} item xs={12} md={9}>
             <Box sx={{  lineHeight: '1', mb: '1rem' }}>
                <Typography
@@ -294,7 +312,7 @@ export default function ConfigurationComponent({
                   size="large"
                   fullWidth
                   disabled={editPass}
-                  onClick={deleteMyAccount}
+                  onClick={handleOpenAlertDelete}
                >
               Borrar mi Cuenta
                </Button>
